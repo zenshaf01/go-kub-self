85e4396df285c8fd51efcef46fdfa27756e1e61c
starlord <zeeshanshafqat1@outlook.com>
2026-03-03 09:57:02 -0500

    Commit 15

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -12,12 +12,14 @@ type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) e
 type App struct {
 	*http.ServeMux
 	shutdown chan os.Signal
+	mw       []MidHandler
 }
 
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...MidHandler) *App {
 	a := &App{
 		ServeMux: http.NewServeMux(),
 		shutdown: shutdown,
+		mw:       mw,
 	}
 	return a
 }
@@ -25,19 +27,19 @@ func NewApp(shutdown chan os.Signal) *App {
 // Handle This handle function is our own handler which wraps the serve mux HandleFunc function
 // This allows us to have our custom handlers to do any logic before or after the ServeMux
 // HandleFunc. This is also necessary if we want to change the signature of our handlers.
-func (a *App) Handle(path string, handler Handler) {
+func (a *App) Handle(path string, handler Handler, mw ...MidHandler) {
+	// since we have 2 sources of the middlewares we need to call this twice.
+	// after both calls, the handler becomes nested under the middleware call chain.
+	handler = wrapMiddleware(mw, handler)
+	handler = wrapMiddleware(a.mw, handler)
+
 	// Define the serve mux handler
 	h := func(w http.ResponseWriter, r *http.Request) {
-
-		//Put logic before our handler. middleware
-
 		if err := handler(r.Context(), w, r); err != nil {
 			// error handling
 			fmt.Println(err)
 			return
 		}
-
-		// put logic after our handler middleware
 	}
 	a.ServeMux.HandleFunc(path, h)
 }
